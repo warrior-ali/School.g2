9572f74b88c500339db2664d585f0478e9e68451
AliKarimi <calirkarimi7@gmail.com>
2021-11-07 23:46:05 +0330

    [FEAT] create layout module

---
+++ app/src/app/layout/layout.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+const ROUTES: Routes = [
+  {
+    path: '',
+    component: NavbarComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      // {
+      //   path: 'products',
+      //   loadChildren: () =>
+      //     import('../products/products.module').then((m) => m.ProductsModule),
+      // },
+      // {
+      //   path: 'auth',
+      //   loadChildren: () =>
+      //     import('../auth/auth.module').then((m) => m.AuthModule),
+      // },
+      // {
+      //   path: 'cart',
+      //   loadChildren: () =>
+      //     import('../cart/cart.module').then((m) => m.CartModule),
+      // },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  exports: [RouterModule],
+  declarations: [NavbarComponent, HomeComponent],
+})
+export class LayoutModule {}
