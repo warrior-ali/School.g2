d9c2c8735d25cbffd539fefcc99cea19c39d79cf
AliKarimi <calirkarimi7@gmail.com>
2021-11-07 23:47:35 +0330

    [FEAT] add navbar component

---
+++ app/src/app/layout/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angular-nestjs-redis-e-shop-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
