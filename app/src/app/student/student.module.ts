083a79cd816f076977aaf2448cd839f687c0063b
AliKarimi <calirkarimi7@gmail.com>
2021-11-09 21:07:10 +0330

    [FEAT] add some needed mat libraries

--- app/src/app/student/student.module.ts
+++ app/src/app/student/student.module.ts
@@ -8,8 +8,16 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
 
 const Material_Module: any[] = [
   MatTabsModule,
@@ -17,6 +25,11 @@ const Material_Module: any[] = [
   MatInputModule,
   MatButtonModule,
   MatSnackBarModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatTableModule,
+  MatIconModule
 ];
 
 @NgModule({
@@ -29,6 +42,7 @@ const Material_Module: any[] = [
     CommonModule,
     StudentRoutingModule,
     Material_Module,
+    FormsModule,
     ReactiveFormsModule,
   ],
 })
