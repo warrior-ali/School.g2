a1d825340a1d5b8a88ccb7698d48b509de095dd7
AliKarimi <calirkarimi7@gmail.com>
2021-11-09 13:20:30 +0330

    [FEAT] make insert form

--- app/src/app/student/components/crud/insert-student/insert-student.component.ts
+++ app/src/app/student/components/crud/insert-student/insert-student.component.ts
@@ -1,46 +1,36 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
+interface GradeLevel {
+  gid: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-insert-student',
   templateUrl: './insert-student.component.html',
-  styleUrls: ['./insert-student.component.scss']
+  styleUrls: ['./insert-student.component.scss'],
 })
 export class InsertStudentComponent implements OnInit {
-
   @Output() changeTab = new EventEmitter<number>();
 
   form = this.fb.group({
-    username: '',
-    password: '',
     first_name: '',
     last_name: '',
-    email: '',
+    date_of_birth: '',
+    grade_level: '',
     phone_number: '',
   });
-  constructor(
-    private readonly fb: FormBuilder,
-    // private readonly authService: AuthService,
-    // private snackBar: MatSnackBar
-  ) {}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
 
-  onRegister() {
-    // this.authService
-    //   .register(this.form.value)
-    //   .subscribe((res: ApiResponse<[]>) => {
-    //     if (res.Success) {
-    //       this.snackBar.open('Register Successfully ... !', 'x', {
-    //         duration: 1500,
-    //       });
-    //       this.changeTab.emit(0);
-    //     } else
-    //       this.snackBar.open('Somthing is wrong. Please try again ... !', 'x', {
-    //         duration: 1500,
-    //       });
-    //   });
-  }
+  gradeLevels: GradeLevel[] = [
+    { gid: '1', name: 'Freshman' },
+    { gid: '2', name: 'Sophomore' },
+    { gid: '3', name: 'Junior' },
+    { gid: '4', name: 'Senior' },
+  ];
 
+  constructor(private readonly fb: FormBuilder) {}
+  ngOnInit(): void {}
+
+  onInsert() {}
 }
